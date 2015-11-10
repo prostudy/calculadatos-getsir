8b6ef1771cc0e1ee2bf7653279ca3571edae9668
prostudy <pmstudykit@gmail.com>
2015-11-10 17:21:09 -0600

    Eliminando archivos incorrectos.

--- CalculaDatos/www/js/ConoceMasCtrl.js
+++
@@ -1,7 +0,0 @@
-angular.module('starter.controllers')
-
-.controller('ConoceMasCtrl', function($scope,ConoceMasService) { 
-	ConoceMasService.getJSONConoceMas().then(function(response){
-	    $scope.concepts = response;
-	  }); 
-});
